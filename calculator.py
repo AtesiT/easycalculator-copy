114ea9ce5b12100e5c9e2e55d0454bcd24e81348
AtesiT <atesit@mail.ru>
2022-10-03 21:28:32 +0300

    add comments and lables and other things

--- calculator.py
+++ calculator.py
@@ -9,8 +9,17 @@ def equals():
 def clear():
     pass
 
-window = Tk()
-window.title('Calculator')
-window.geometry('500x500')
+window = Tk() #Вывод оконного интерфейса
+window.title('My calculator') #Заголовок
+window.geometry('500x500') #Размер окна
 
-window.mainloop()
+equation_text = ""
+
+equation_label = StringVar() #Строка 
+
+label = Label(window,  textvariable = equation_label, font=('consolas', 20), bg='white', width=24, height=2) #окно,  , #шрифт, #задний фон, #ширина, #длина, 
+label.pack()
+
+window.mainloop() #цикл окна, чтобы дальше код не работал, пока не закроется окно
+
+print('The window was closed by user')
