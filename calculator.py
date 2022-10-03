42f64f9fb9f72ac25e858275954a74b37f35dcc0
AtesiT <atesit@mail.ru>
2022-10-03 19:02:45 +0300

    add three functions and window from tkinter

---
+++ calculator.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+def button_press(num):
+    pass
+
+def equals():
+    pass
+
+def clear():
+    pass
+
+window = Tk()
+window.title('Calculator')
+window.geometry('500x500')
+
+window.mainloop()
