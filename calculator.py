14f19e5f13fe2a3c964fe950d8dc9f757e06f3ad
AtesiT <atesit@mail.ru>
2022-10-04 23:51:47 +0300

    add buttons

--- calculator.py
+++ calculator.py
@@ -18,7 +18,48 @@ equation_text = ""
 equation_label = StringVar() #Строка 
 
 label = Label(window,  textvariable = equation_label, font=('consolas', 20), bg='white', width=24, height=2) #окно,  , #шрифт, #задний фон, #ширина, #длина, 
-label.pack()
+label.pack() 
+
+frame = Frame(window) #рамка
+frame.pack()
+
+button1 = Button(frame, text=1, height=4, width=9, font=35, 
+                command = lambda: button_press(1) ) #первая кнопка
+button1.grid(row=0,column=0) # "сетка" и расположение кнопки
+
+button2 = Button(frame, text=2, height=4, width=9, font=35, 
+                command = lambda: button_press(2) ) #вторая кнопка
+button2.grid(row=0,column=1) # "сетка" и расположение кнопки
+
+button3 = Button(frame, text=3, height=4, width=9, font=35, 
+                command = lambda: button_press(3) ) #третья кнопка
+button3.grid(row=0,column=2) # "сетка" и расположение кнопки
+
+button4 = Button(frame, text=4, height=4, width=9, font=35, 
+                command = lambda: button_press(4) ) #четвертая кнопка
+button4.grid(row=1,column=0) # "сетка" и расположение кнопки
+
+button5 = Button(frame, text=5, height=4, width=9, font=35, 
+                command = lambda: button_press(5) ) #пятая кнопка
+button5.grid(row=1,column=1) # "сетка" и расположение кнопки
+
+button6 = Button(frame, text=6, height=4, width=9, font=35, 
+                command = lambda: button_press(6) ) #шестая кнопка
+button6.grid(row=1,column=2) # "сетка" и расположение кнопки
+
+button7 = Button(frame, text=7, height=4, width=9, font=35, 
+                command = lambda: button_press(7) ) #седьмая кнопка
+button7.grid(row=2,column=0) # "сетка" и расположение кнопки
+
+button8 = Button(frame, text=8, height=4, width=9, font=35, 
+                command = lambda: button_press(8) ) #восьмая кнопка
+button8.grid(row=2,column=1) # "сетка" и расположение кнопки
+
+button9 = Button(frame, text=9, height=4, width=9, font=35, 
+                command = lambda: button_press(9) ) #девятая кнопка
+button9.grid(row=2,column=2) # "сетка" и расположение кнопки
+
+
 
 window.mainloop() #цикл окна, чтобы дальше код не работал, пока не закроется окно
 
